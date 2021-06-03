fd35a788935ebe1945759d65cb59520c88f151bb
Marian01. <msanchezv2011@gmail.com>
2021-06-02 20:03:29 -0500

    mi primer repositorio

---
+++ src/main/java/com/empresa/Sesion09bApplication.java
@@ -0,0 +1,13 @@
+package com.empresa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sesion09bApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sesion09bApplication.class, args);
+	}
+
+}
